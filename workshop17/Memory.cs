23db1092be853c279bd7a9eb97e3c94c7ac306c2
kbsim <keebaiksim@gmail.com>
2015-11-29 20:36:14 -0500

    added some code fo the memory constructor

--- workshop17/Memory.cs
+++ workshop17/Memory.cs
@@ -13,21 +13,33 @@ namespace workshop17
         Vector4 Location;
         int id;
         //List<images> frames; // each index represents a collection of snapshots or photos taken of the person
+        List<int> frames;
         int currentFrame; // the index of the snapshot that should be displayed next time render is called
-       
+        int snapshot;
+        
         // Constructor
         public Memory() // parameters?
         {
-           // initialize instance variables & other setup
+            // initialize instance variables & other setup 
+            // code that runs only once at the begining of the program goes here
+            timeCreated = DateTime.Now;
+            frames = new List<int>();
+            currentFrame = 0;
+
+
         }
 
+       
         // Function add
         //  This function is called to add a ‘snapshot’ to our array of frames. This should only be called from a person object.
         // Parameters
         //  image - an image that we want to add to this memory
-        public void add() // parameters?
+        public void add(int snapshot) // parameters?
         {
+            List<int> frames = new List<int>();
 
+
+            frames.Add(snapshot);
         }
 
 
