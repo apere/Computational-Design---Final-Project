22b546b1349aead37e8acdebf738d512cfed2ca0
Enol Vallina <enol.vallina@outlook.com>
2015-12-04 22:27:57 -0500

    Closest person + Compare Function + Orientation

---
+++ workshop17/Memory.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.Kinect;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Graphics;
+
+namespace workshop17
+{
+    public class Memory
+    {
+        DateTime timeCreated;
+        Vector3d Location;
+        int id;
+        //List<images> frames; // each index represents a collection of snapshots or photos taken of the person
+        int currentFrame; // the index of the snapshot that should be displayed next time render is called
+       
+        // Constructor
+        public Memory() // parameters?
+        {
+           // initialize instance variables & other setup
+        }
+
+        // Function add
+        //  This function is called to add a ‘snapshot’ to our array of frames. This should only be called from a person object.
+        // Parameters
+        //  image - an image that we want to add to this memory
+        public void add() // parameters?
+        {
+
+        }
+
+
+        // Function getClosestPerson
+        //  This function loops through the supplied array of Person objects and 
+        //      returns the one physically closest to this memory
+        // Parameters
+        //  persons - a list of person objects
+        // Return
+        //  the person in persons that is physically closest to this memory
+        public Person getClosestPerson(List<Person> persons)
+        {  
+            Person closestPerson=persons[0];
+            Vector3d position = closestPerson.getPosition();
+            float dist = (float)Math.Sqrt(Math.Pow(position.X + Location.X, 2) + Math.Pow(position.Y + Location.Y, 2) + Math.Pow(position.Z + Location.Z, 2)); ;
+            float closestDist = dist;
+
+            foreach (Person p in persons)
+            {
+                position = p.getPosition();
+                dist = (float)Math.Sqrt(Math.Pow(position.X + Location.X,2) + Math.Pow(position.Y + Location.Y,2) + Math.Pow(position.Z + Location.Z,2));
+                if (dist < closestDist)
+                {
+                    closestDist = dist;
+                    closestPerson = p;
+                }
+            }
+            // must return a person
+            return closestPerson;
+        }
+
+        // Function getOrientation
+        //
+        // Return
+        //  A Vector pointing in the direction that we want to face
+        public Vector3d getOrientation(List<Person> persons)
+        {
+            return this.getClosestPerson(persons).getPosition().Normalized();
+        }
+
+        // Function render
+        // This function should be called every time we want to display and update the memory. 
+        // This should calculate the correct orientation and display the current frame.
+        public void render(List<Person> persons)
+        {
+
+        }
+
+    }
+}
---
+++ workshop17/Person.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.Kinect;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Graphics;
+
+namespace workshop17
+{
+    public class Person
+    {
+        protected Memory myMemory;
+        protected ulong id;
+        protected Body myBody; // reference to the body object that created this person
+
+        // constructor
+        public Person()
+        {
+            // initialize instance variables & other setup
+        }
+
+        // Function takeSnapshot
+        //  This function takes a snapshot of the person as is 
+        //      (still unsure whether this is an picture, colored points, whatever) 
+        //      and adds that snapshot to the person’s memory.
+        public void takeSnapshot()
+        {
+
+        }
+
+        // Function getMemory
+        //
+        // Returns
+        //  this person's memory object
+        public Memory getMemory()
+        {
+            return myMemory;
+        }
+
+
+        // Function compare
+        //  This function takes in a Body object & returns true if it is the body
+        //      that corresponds to this person and false otherwise
+        //
+        // Parameters
+        // Body Skeleton - the Body object we are comparing with
+        //
+        // Return
+        //  whether or not the skeleton belongs to this person
+        public bool compare(Body Skeleton)
+        {
+            if (Skeleton != null)
+            {
+                return this.id == Skeleton.TrackingId;
+            }
+            return false;
+
+        }
+        public Vector3d getPosition() {
+            Vector3d ps=new Vector3d(myBody.Joints[0].Position.X, myBody.Joints[0].Position.Y, myBody.Joints[0].Position.Z);
+            return ps;
+        }
+    }
+}
