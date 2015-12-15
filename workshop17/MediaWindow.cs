665b8ce962fd79e01b9e52b0085e62ad9cb801e9
Adam Pere <adampere001@gmail.com>
2015-12-15 16:57:57 -0500

    updated comments etc

--- workshop17/MediaWindow.cs
+++ workshop17/MediaWindow.cs
@@ -11,34 +11,47 @@ using Microsoft.Kinect;
 
 using System.Windows.Forms;
 
+/// <summary>
+/// Media Window
+/// Authors: Adam M Pere, Keebaik Sim, Enol Vallina
+/// Introduction to Computational Design - Fall 2015, Harvard Graduate School of Design
+/// 
+/// The MediaWindow class is where the OpenGL scene is set up and where the main communication with the Kinect Sensor happens.
+/// </summary>
+
 namespace workshop17
 {
     public class MediaWindow
     {
-        public int Width = 0;       //width of the viewport in
+        public int Width = 0;       //width of the viewport in pixels
         public int Height = 0;      //height of the viewport in pixels
         public double MouseX = 0.0; //location of the mouse along X
         public double MouseY = 0.0; //location of the mouse along Y
 
+        public KinectHelper kinect = new KinectHelper(true, true, true);  // wrapper object for kinect sensor
+        public List<Person> persons = new List<Person>();                 // list of people currently standing in front of kinect sensor
+        public List<Memory> memories = new List<Memory>();                // list of the 'memories' of people who once stood in front of the kinect sensor
 
-        public KinectHelper kinect = new KinectHelper(true, true, true);
-        public List<Person> persons = new List<Person>();
-        public List<Memory> memories = new List<Memory>();
+        public List<List<Vector3d>> allJoints = new List<List<Vector3d>>();         // list of all joints currently being detected - separated by user
+        public List<List<KinectPoint>> allPoints = new List<List<KinectPoint>>();   // list of all points within a certain distance of a joint - separated by user
 
-
-
-
-        //initialization function. Everything you write here is executed once in the beginning of the program
+        /// <summary>
+        /// Initialize
+        /// </summary>
         public void Initialize()
         {
             kinect.Initialize();
         }
 
+        /// <summary>
+        /// Terminate
+        /// </summary>
         public void Terminate()
         {
             kinect.Close();
         }
 
+        // Camera Variables
         public double AngleXZ = 300.0;
         public double AngleY = 0.0;
         public double Distance = 4.0;
@@ -47,13 +60,17 @@ namespace workshop17
         public Vector3d Target=new Vector3d(0.0, 0.0, 2.0);
         public Vector3d Up = new Vector3d(0.0, 1.0, 0.0);
 
-        public List<List<Vector3d>> allJoints = new List<List<Vector3d>>();
-        public List<List<KinectPoint>> allPoints = new List<List<KinectPoint>>();
+        double mouseX0 = 0.0;
+        double mouseY0 = 0.0;
 
-        //animation function. This contains code executed 20 times per second.
+
+        /// <summary>
+        /// onFrameUpdate
+        /// This is the animation function. It is called ~20x per second to update the screen
+        /// </summary>
         public void OnFrameUpdate()
         {
-
+            // Scene Setup
             GL.ClearColor(1f, 1f, 1f, 1.0f);
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
@@ -69,7 +86,6 @@ namespace workshop17
             Matrix4d vmat = Matrix4d.LookAt(Eye, Target, Up);
             GL.LoadMatrix(ref vmat);
 
-
             GL.Enable(EnableCap.Lighting); //enable lighting calculations
             GL.Enable(EnableCap.Light0);    //enable the first light
             GL.Light(LightName.Light0, LightParameter.Position, new OpenTK.Vector4((float)Target.X, (float)Target.Y, (float)Target.Z + 20, 2.0f)); //set light position and color
@@ -82,6 +98,7 @@ namespace workshop17
             GL.LightModel(LightModelParameter.LightModelTwoSide, 1); //set lighting model 
             GL.LightModel(LightModelParameter.LightModelLocalViewer, 1);
 
+
             // Any new people standing in front of the kinect?
             if (kinect.HasSkeletonData && kinect.HasDepthData)
             {
@@ -89,13 +106,13 @@ namespace workshop17
                 allPoints = findPoints();
             }
 
-            // Are we keeping track of anyone that is no longer in front of the kinect?
+            // Remove or take a snapshot of any person currently in front of the kinect
             removeOrSnap();
 
             // Render each memory
-            foreach (Memory mem in memories) // display each memory
+            foreach (Memory mem in memories)
             {
-                mem.render(persons); // call the render method for each memory so they can draw themselves to the screen
+                mem.render();
             }
 
             Console.WriteLine(" ");
@@ -105,10 +122,13 @@ namespace workshop17
             allJoints.Clear();
             //allPoints.Clear();
 
-        } // END onFrameUpdate()
+        } 
 
-        double mouseX0 = 0.0;
-        double mouseY0 = 0.0;
+
+        /// <summary>
+        /// MouseMove
+        /// Use the mouse to change the camera's view
+        /// </summary>
         public void MouseMove(double x, double y, MouseButtons button)
         {
             mouseX0 = MouseX;
@@ -131,10 +151,10 @@ namespace workshop17
             }
         }
 
-
-        // Function findPoints
-        //  
-        // Returns a list of kinect points associated with all joint data stored in the variable allJoints
+        /// <summary>
+        /// findPoints
+        /// </summary>
+        /// <return>a list of kinect points associated with all joint data stored in the variable allJoints</return> 
         public List<List<KinectPoint>> findPoints()
         {
             List<List<KinectPoint>> allKPoints = new List<List<KinectPoint>>();
@@ -142,23 +162,24 @@ namespace workshop17
             KinectPoint kp;
             bool add;
             double dist;
+            int step = 3;
 
-            if (allJoints != null && allJoints.Count > 0)
+            if (allJoints != null && allJoints.Count > 0) // error checking
             {
-                foreach (List<Vector3d> jointPoints in allJoints) // For every user's joints
+                foreach (List<Vector3d> jointPoints in allJoints) // loop through set of joints (each set is one user's joints)
                 {
                     tempPoints = new List<KinectPoint>();
                     allKPoints.Add(tempPoints);
 
-                    if (jointPoints != null && jointPoints.Count > 0) // if they have joints
+                    if (jointPoints != null && jointPoints.Count > 0) // error checking
                     {
-                        for (int j = 0; j < kinect.DepthHeight; j += 3) // find the kinect points closest to them
+                        for (int j = 0; j < kinect.DepthHeight; j += step) // loop through every kinect point, saving the ones within a certain distance
                         {
-                            for (int k = 0; k < kinect.DepthWidth; k += 3)
+                            for (int k = 0; k < kinect.DepthWidth; k += step)
                             {
                                 kp = kinect.Points[k, j];
                                 add = false;
-                                foreach (Vector3d jointPoint in jointPoints)
+                                foreach (Vector3d jointPoint in jointPoints) // loop through every joint of this user comparing distance to point
                                 {
                                     if (!add)
                                     {
@@ -183,11 +204,11 @@ namespace workshop17
             return allKPoints;
         }
 
-
-        // Function addSkeletons
-        //
-        // Gets the joint data for every person object ---- may have to do this after add (or remove).
-        // If it finds a person who has just been detected by the kinect this frame, it will add them to our list of persons
+        /// <summary>
+        /// addSkeletons
+        /// Gets the joint data for every person object ---- may have to do this after add (or remove).
+        /// If it finds a person who has just been detected by the kinect this frame, it will add them to our list of persons
+        /// </summary>
         public void addSkeletons()
         {
             bool identified = false;
@@ -215,17 +236,17 @@ namespace workshop17
             }
         }
 
-        // Function removeOrSnap
-        //
-        // This function either removes the person object (and stores their memory) of a person no longer being tracked by the kinect 
-        // OR it invokes taking a snapshot of that person
-        
+        /// <summary>
+        /// removeOrSnap
+        /// This function either removes the person object (and stores their memory) of a person no longer being tracked by the kinect 
+        /// OR it invokes taking a snapshot of that person
+        /// </summary>
         public void removeOrSnap()
         {
             bool removed = true;
             List<Person> toRemove = new List<Person>();
             Person pp;
-            for (int i = 0; i < persons.Count; i++) // check to see if we are keeping track of people that are no longer in front of the installation and should be turned into memories
+            for (int i = 0; i < persons.Count; i++) // check to see if we are keeping track of people that are no longer in front of the installation and should be removed (turned into memories)
             {
                 pp = persons[i];
                 foreach (Body skeleton in kinect.Bodies)
@@ -259,6 +280,5 @@ namespace workshop17
             }
             toRemove.Clear();
         }
-
     }
 }
--- workshop17/Memory.cs
+++ workshop17/Memory.cs
@@ -9,89 +9,154 @@ using OpenTK;
 using OpenTK.Graphics.OpenGL;
 using OpenTK.Graphics;
 
-
+/// <summary>
+/// Memory Class
+/// Authors: Adam M Pere, Keebaik Sim, Enol Vallina
+/// Introduction to Computational Design - Fall 2015, Harvard Graduate School of Design
+/// 
+/// The memory class represents a recording of people that were once standing in front of the kinect sensor but are no longer.
+/// The 'memory' of a person object.
+/// </summary>
 namespace workshop17
 {
     public class Memory
     {
         DateTime timeCreated;
-        Vector3d Location;
         ulong id;
-        //List<images> frames; // each index represents a collection of snapshots or photos taken of the person
         int currentFrame; // the index of the snapshot that should be displayed next time render is called
-        List<List<KinectPoint>> snapshots;
+        List<List<KinectPoint>> snapshots; // visual representation of memory
 
-        // Constructor
-        public Memory(ulong i) // parameters?
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="i">The ID associated with this memory. It should be the same ID as the person object this memory belongs to and the kinect skeleton associated with that body</param>
+        public Memory(ulong i)
         {
             timeCreated = DateTime.Now;
-            // initialize instance variables & other setup
             snapshots = new List<List<KinectPoint>>();
             currentFrame = 0;
             id = i;
         }
 
-        // Function add
-        //  This function is called to add a ‘snap shot’ to our array of frames. This should only be called from a person object.
-        // Parameters
-        //  image - an image that we want to add to this memory
+        /// <summary>
+        /// getID
+        /// </summary>
+        /// <returns>The ID of this memory object</returns>
+        public ulong getID()
+        {
+            return id;
+        }
+
+        /// <summary>
+        /// getTimeCreated
+        /// </summary>
+        /// <returns>A DateTime object representing the time this memory was first instantiated</returns>
+        public DateTime getTimeCreated()
+        {
+            return timeCreated;
+        }
+
+        /// <summary>
+        /// getTimeSinceCreated
+        /// </summary>
+        /// <returns>A TimeSpan object representing the amount of time that has passed since this object was created</returns>
+        public TimeSpan getTimeSinceCreated()
+        {
+            return DateTime.Now.Subtract(timeCreated);
+        }
+
+        /// <summary>
+        /// getNumberOfFrames
+        /// </summary>
+        /// <returns>the number of frames contained in this memory</returns>
+        public int getNumberOfFrames()
+        {
+            return snapshots.Count;
+        }
+
+        /// <summary>
+        /// getCurrentFrameIndex
+        /// </summary>
+        /// <returns>the index of the currently displayed frame</returns>
+        public int getCurrentFrameIndex()
+        {
+            return currentFrame;
+        }
+        
+        /// <summary>
+        /// getLocation
+        /// </summary>
+        /// <returns>The location of this memory = the Vector3d of the first point in the current frame</returns>
+        public Vector3d getLocation()
+        {
+            double offset = getTimeSinceCreated().TotalSeconds / 20;
+            return new Vector3d(snapshots[currentFrame][0].p.X, snapshots[currentFrame][0].p.Y, snapshots[currentFrame][0].p.Z + offset);
+        }
+
+        /// <summary>
+        /// add
+        /// This function is called to add a ‘snapshot’ to our array of frames. This should only be called from a person object.
+        /// </summary>
+        /// <param name="snapshotPoints">A list of Kinect points that represent an image that we want to add to this memory</param>
         public void add(List<KinectPoint> snapshotPoints) // parameters?
         {
             snapshots.Add(snapshotPoints);
         }
 
 
-        // Function getClosestPerson
-        //  This function loops through the supplied array of Person objects and 
-        //      returns the one physically closest to this memory
-        // Parameters
-        //  persons - a list of person objects
-        // Return
-        //  the person in persons that is physically closest to this memory
+        /// <summary>
+        /// getClosestPerson
+        /// This function loops through the supplied array of Person objects and finds the one physically closest to this memory. --- may not be using anymore
+        /// </summary>
+        /// <param name="persons">a list of person objects that we want to check within<param>
+        /// <returns>the person in 'persons' that is physically closest to this memory</returns>
         public Person getClosestPerson(List<Person> persons)
         {  
             Person closestPerson=persons[0];
             Vector3d position = closestPerson.getPosition();
-            float dist = (float)Math.Sqrt(Math.Pow(position.X + Location.X, 2) + Math.Pow(position.Y + Location.Y, 2) + Math.Pow(position.Z + Location.Z, 2)); ;
-            float closestDist = dist;
+            Vector3d location = getLocation();
+            double dist = Math.Abs(Vector3d.Subtract(position, location).Length);
+            double closestDist = dist;
 
             foreach (Person p in persons)
             {
                 position = p.getPosition();
-                dist = (float)Math.Sqrt(Math.Pow(position.X + Location.X,2) + Math.Pow(position.Y + Location.Y,2) + Math.Pow(position.Z + Location.Z,2));
+                dist = Math.Abs(Vector3d.Subtract(position, location).Length);
                 if (dist < closestDist)
                 {
                     closestDist = dist;
                     closestPerson = p;
                 }
             }
-            // must return a person
             return closestPerson;
         }
 
-        public int Count()
-        {
-            return snapshots.Count;
-        }
-
-        // Function getOrientation
-        //
-        // Return
-        //  A Vector pointing in the direction that we want to face
+        /// <summary>
+        /// getOrientation
+        /// Finds the direction facing the closest person
+        /// </summary>
+        /// <param name="persons"></param>
+        /// <returns>A Vector pointing in the direction that we want to face</returns>
         public Vector3d getOrientation(List<Person> persons)
         {
             return this.getClosestPerson(persons).getPosition().Normalized();
         }
 
-        // Function render
-        // This function should be called every time we want to display and update the memory. 
-        // This should calculate the correct orientation and display the current frame.
-        public void render(List<Person> persons)
+        /// <summary>
+        /// render
+        /// This function should be called every time we want to display and update the memory. 
+        /// This function draws the current frame (snapshot) to the screen.
+        /// 
+        /// The longer since timeCreated, the larger the z-index offset (frame pushed back)
+        /// </summary>
+        public void render()
         {
             List < KinectPoint > currShot = snapshots[currentFrame];
             double zIndex = currShot[0].p.Z;
-            double offset = (DateTime.Now - timeCreated).TotalSeconds / 20; // time offset for z-axis
-            double buzz = (DateTime.Now - timeCreated).Seconds/20;
+            double offset = getTimeSinceCreated().TotalSeconds / 20; // time offset for z-axis
+            double buzz = getTimeSinceCreated().Seconds / 20;
+
+       
 
             GL.PointSize(4);  // Changing point size gives some cool abstract results
             GL.Enable(EnableCap.DepthTest);
@@ -100,22 +165,21 @@ namespace workshop17
             // PrimitiveType.QuadStrip -- even cooler
             // PrimitiveType.TriangleFan -- weird but preserves face
             // PrimitiveType.Triangles -- weird noisy silhouette
-            // 
-            Random rand = new Random();
+            
             
             foreach (KinectPoint kp in currShot)
             {
                 GL.Color4(kp.color);
                 GL.Vertex3(kp.p.X,
                     kp.p.Y,
-                    kp.p.Z + offset);
+                    kp.p.Z + offset); // time-offset
             }
             GL.End();
            
-            //Console.WriteLine("mem" + id + " frame:" + currentFrame);
+            //Console.WriteLine("mem" + id + " frame:" + currentFrame); // memory rendering debugging
         
             currentFrame = currentFrame + 1;
-            if(currentFrame >= snapshots.Count)
+            if(currentFrame >= snapshots.Count) // start over ?
             {
                 currentFrame = 0;
             } 
